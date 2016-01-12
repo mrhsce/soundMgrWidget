a71f36b963019e35911a4ba1a43f792bc38973ab
MrHsCE <mrhsce@gmail.com>
2016-01-13 00:32:04 +0330

    [add] proximity sensor test has been implemented

--- app/src/main/java/com/androtec/mrhs/frontspeakertest/MainActivity.java
+++ app/src/main/java/com/androtec/mrhs/frontspeakertest/MainActivity.java
@@ -1,85 +1,133 @@
 package com.androtec.mrhs.frontspeakertest;
 
 import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.media.AudioManager;
-import android.media.MediaPlayer;
+import android.media.ToneGenerator;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.Toast;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements SensorEventListener{
 
-    Switch frontSpeakerSwitch;
     AudioManager audioManager;
-    Boolean frontSpeakerActivated;
-    public MediaPlayer m_mpSpeakerPlayer;
+    Switch frontSpeakerSwitch;
 
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        frontSpeakerSwitch = (Switch)findViewById(R.id.frontSpeakerSwitch);
-        audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
-        Log.d("Main",Integer.toString(audioManager.getMode()));
-        if(audioManager.getMode() == AudioManager.MODE_IN_CALL){
-            frontSpeakerActivated = true;
-            frontSpeakerSwitch.setChecked(true);
-        }
-        else{
-            frontSpeakerActivated = false;
-            frontSpeakerSwitch.setChecked(false);
-        }
+        pm = this.getPackageManager();
+        audioManager = (AudioManager) this.getSystemService(AUDIO_SERVICE);
+        frontSpeakerSwitch = (Switch) findViewById(R.id.frontSpeakerSwitch);
 
+
+        updateSwitchState();
+
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+
+        //audioManager.setRouting(AudioManager.MODE_NORMAL, AudioManager.ROUTE_EARPIECE, AudioManager.ROUTE_ALL);
+
+
+        Log.d("in call" ,Integer.toString(AudioManager.MODE_IN_CALL ));
+        Log.d("MODE_IN_COMMUNICATION" ,Integer.toString(AudioManager.MODE_IN_COMMUNICATION ));
+        Log.d("MODE_NORMAL" ,Integer.toString(AudioManager.MODE_NORMAL ));
+        Log.d("MODE_INVALID" ,Integer.toString(AudioManager.MODE_INVALID ));
+        Log.d("MODE_RINGTONE" ,Integer.toString(AudioManager.MODE_RINGTONE ));
+        Log.d("MODE_CURRENT" ,Integer.toString(AudioManager.MODE_CURRENT ));
+        Log.d("ROUTE_EARPIECE" ,Integer.toString(AudioManager.ROUTE_EARPIECE));
+        Log.d("ROUTE_ALL", Integer.toString(AudioManager.ROUTE_ALL ));
 
         frontSpeakerSwitch.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View compoundButton){
-                frontSpeakerActivated = !frontSpeakerActivated;
-                if (frontSpeakerActivated) {
-                    frontSpeakerSwitch.setChecked(true);
-//                    m_mpSpeakerPlayer.
-                    audioManager.setMode(AudioManager.MODE_IN_COMMUNICATION);
-                    audioManager.setSpeakerphoneOn(false);
-                    Log.d("onclick","front speaker turned on");
-                    Log.d("Main",Integer.toString(audioManager.getMode()));
-                    Toast.makeText(MainActivity.this,"set to earpiece",Toast.LENGTH_LONG).show();
-//                    audioManager.setSpeakerphoneOn(false);
-//
-//                    audioManager.setRouting(AudioManager.MODE_NORMAL, AudioManager.ROUTE_EARPIECE, AudioManager.ROUTE_ALL);
-//
-//
-//                    setVolumeControlStream(AudioManager.STREAM_VOICE_CALL);
-//
-//
-//                    audioManager.setMode(AudioManager.MODE_IN_CALL);
-//
-//                    m_mpSpeakerPlayer.reset();
-//                    try {
-//                        m_mpSpeakerPlayer.setDataSource("sdcard/90.mp3");
-//                        m_mpSpeakerPlayer.prepare();
-//                    }
-//                    catch (io)
-//
-//                    m_mpSpeakerPlayer.start();
-                } else {
-                    frontSpeakerSwitch.setChecked(false);
-                    audioManager.setMode(AudioManager.MODE_NORMAL);
-                    audioManager.setSpeakerphoneOn(true);
-                    Log.d("onclick","front speaker turned off");
-                    Log.d("Main",Integer.toString(audioManager.getMode()));
-                    Toast.makeText(MainActivity.this,"set to speaker",Toast.LENGTH_LONG).show();
+            public void onClick(View view) {
+                if (audioManager.getMode() == 0)
+                {
+                    setEarpiece(true);
+                }
+                else
+                {
+                    setEarpiece(false);
                 }
             }
         });
+    }
 
 
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mSensor,
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        ToneGenerator toneG = new ToneGenerator(AudioManager.STREAM_ALARM, 100);
+        toneG.startTone(ToneGenerator.TONE_CDMA_ALERT_CALL_GUARD, 200);
+        Toast.makeText(this,"sensor changed",Toast.LENGTH_LONG);
+    }
+
+    private PackageManager pm ;
+
+    public boolean haveTelephony()
+    {
+        return pm.hasSystemFeature("android.hardware.telephony");
+    }
+
+    public void setEarpiece(boolean flag)
+    {
+        byte byte0 = AudioManager.MODE_IN_CALL;
+        if (!haveTelephony()) {
+            return;
+        }
+        audioManager.setSpeakerphoneOn(false);
+        if (flag)
+        {
+            audioManager.setMode(byte0);
+            audioManager.setSpeakerphoneOn(false);
+        } else
+        {
+            audioManager.setMode(0);
+            audioManager.setRouting(AudioManager.MODE_NORMAL, 2, -1);
+
+        }
+        updateSwitchState();
+
+    }
+
+    void updateSwitchState()
+    {
+
+        if(audioManager.getMode() == AudioManager.MODE_IN_CALL || audioManager.getMode() == AudioManager.MODE_IN_COMMUNICATION){
+            frontSpeakerSwitch.setChecked(true);
+        }
+        else {
+            frontSpeakerSwitch.setChecked(false);
+        }
     }
 
 
